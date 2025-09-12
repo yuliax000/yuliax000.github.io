7f02cb63f8aea6ff1280c1d736d0d9b4ae26d8b0
yuliax000 <ArthyerX@gmail.com>
2025-09-12 12:18:00 +1000

    unfinished setting up

--- mediaPlayer/main.js
+++ mediaPlayer/main.js
@@ -1,5 +1,13 @@
+/*find the elements i want to interact with */
+const videoElement = document.querySelector("#mediaPlayer");
+const playPauseButton = document.querySelector("#playPauseButton");
+const timeline = document.querySelector("#timelineProgress");
+
+/* when js loads remove default controls*/
+videoElement.removeAttribute("controls");
+
 /*
-play pasue button behaviour:
+play pause button behaviour:
 if media is not playing--when I click it begins the playback of the media file
 if media is playing---when I click again it pause the playback of the media file
 feedback:
@@ -7,7 +15,26 @@ toggle icon based on playing state
 cursor change on hover
 */
 
+function playPause() {
+  if (videoElement.paused || videoElement.ended) {
+    videoElement.play();
+    playPauseButton.textContent = "pause unicode";
+  } else {
+    videoElement.pause();
+    playPauseButton.textContent = "play unicode";
+  }
+}
+playPauseButton.addEventListener("click", playPause);
+
 /* timelinee behavior:
 it should update as media playback occurss to show current time
 I should be able to click and jump to particular time
 */
+
+function updateTimeline() {
+  /*find percentage of total time */
+  let timePercent = (videoElement.currentTime / videoElement.duration) * 100;
+  timeline.value = timePercent;
+}
+
+videoElement.addEventListener("timeupdate", updateTimeline);
