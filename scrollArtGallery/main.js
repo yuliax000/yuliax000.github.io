479d3417e159028d07f95cd0a62665bae1ada84e
yuliax000 <ArthyerX@gmail.com>
2025-10-16 20:02:06 +1100

    change layout & adding function

---
+++ scrollArtGallery/main.js
@@ -0,0 +1,23 @@
+// I tried the "wheel" method which contained in the reference video
+// sadly it didn't work well, it's not smooth at all.
+// then I turned into "scroll"
+
+// let imgs = document.querySelector(`.imgs`);
+
+// function next() {
+//   let img = documents.querySelectorAll(`img`);
+//   imgs.appendChild(img[0]);
+// }
+
+// function prev() {
+//   let img = document.querySelector(`img`);
+//   imgs.appendChild(img[imgs.length - 1]);
+// }
+
+// window.addEventListener("wheel", function (event) {
+//   if (event.deltaY > 0) {
+//     next();
+//   } else {
+//     prev();
+//   }
+// });
