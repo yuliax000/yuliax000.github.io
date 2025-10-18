20ee90f46d5db3ef8aedb70eec96649d64641c34
yuliax000 <ArthyerX@gmail.com>
2025-10-18 18:14:29 +1100

    fake infinite scroll

--- scrollArtGallery/main.js
+++ scrollArtGallery/main.js
@@ -1,7 +1,7 @@
 // I tried the "wheel" method which contained in the reference video
 // sadly it didn't work well, it's not smooth at all.
 // then I turned into "scroll"
-
+// -------------------------------------------------------------------------------------------
 // let imgs = document.querySelector(`.imgs`);
 
 // function next() {
@@ -21,3 +21,67 @@
 //     prev();
 //   }
 // });
+
+// -------------new method tried to figure out the infinite scroll--------------
+const imgContainer = document.querySelector(".imgs");
+let imgs = Array.from(imgContainer.querySelectorAll("img"));
+
+imgContainer.prepend(...imgs.map((img) => img.cloneNode(true)));
+imgContainer.append(...imgs.map((img) => img.cloneNode(true)));
+imgs = Array.from(imgContainer.querySelectorAll("img"));
+
+const groupCount = imgs.length / 3;
+
+const left0 = imgs[0].offsetLeft;
+const left1 = imgs[1].offsetLeft;
+const gap = Math.max(0, left1 - left0 - imgs[0].offsetWidth);
+const imgWidth = imgs[0].offsetWidth + gap;
+const groupWidth = imgWidth * groupCount;
+
+// setting default shows the second group
+imgContainer.scrollLeft = groupWidth;
+
+let isJumping = false;
+// scroll to certain distance, jump to another group
+function scrollEffect() {
+  if (isJumping) return;
+
+  const leftThreshold = groupWidth * 0.5;
+  const rightThreshold = groupWidth * 1.5;
+  if (imgContainer.scrollLeft < leftThreshold) {
+    isJumping = true;
+    imgContainer.scrollLeft += groupWidth;
+    requestAnimationFrame(() => (isJumping = false));
+  } else if (imgContainer.scrollLeft > rightThreshold) {
+    isJumping = true;
+    imgContainer.scrollLeft -= groupWidth;
+    requestAnimationFrame(() => (isJumping = false));
+  }
+
+  // find where the center is now
+  const centerX = imgContainer.scrollLeft + imgContainer.clientWidth / 2;
+  const halfCWidth = imgContainer.clientWidth / 2;
+
+  // set the blur, scale and opacity
+  imgs.forEach((img) => {
+    const imgCenter = img.offsetLeft + img.offsetWidth / 2;
+    const distance = Math.abs(centerX - imgCenter);
+    //   ratio of the distance to center and half the container width (for calculate the transition)
+    const ratio = Math.max(0, 1 - distance / halfCWidth);
+
+    // calculate the scale, opacity and blur's transition based on distance ratio
+    const scale = 0.7 + ratio * 0.5;
+    const opacity = 0.4 + ratio * 0.6;
+    const blurRem = (1 - ratio) * 5;
+
+    img.style.transform = `scale(${scale})`;
+    img.style.opacity = `${opacity}`;
+    img.style.filter = `blur(${blurRem}rem)`;
+  });
+}
+
+scrollEffect();
+
+imgContainer.addEventListener("scroll", () => {
+  scrollEffect();
+});
