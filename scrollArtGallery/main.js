052609890759079839b2b1815df354a8344965f8
yuliax000 <ArthyerX@gmail.com>
2025-10-22 20:21:22 +1100

    tried 3rd method

--- scrollArtGallery/main.js
+++ scrollArtGallery/main.js
@@ -42,9 +42,32 @@ const groupWidth = imgWidth * groupCount;
 imgContainer.scrollLeft = groupWidth;
 
 let isJumping = false;
-// scroll to certain distance, jump to another group
+
+//   make vertical scroll can control the picture's horizontal scroll
+
+// scroll to the edge of the 3rd / 1st group, jump to 2nd group
 function scrollEffect() {
   if (isJumping) return;
+
+  const leftThreshold = groupWidth * 0.5;
+  const rightThreshold = groupWidth * 1.5;
+  if (imgContainer.scrollLeft < leftThreshold) {
+    isJumping = true;
+    imgContainer.style.scrollBehavior = "auto";
+    imgContainer.scrollLeft += groupWidth;
+    imgContainer.style.scrollBehavior = "smooth";
+    setTimeout(() => (isJumping = false), 0);
+  } else if (imgContainer.scrollLeft > rightThreshold) {
+    isJumping = true;
+    imgContainer.style.scrollBehavior = "auto";
+    imgContainer.scrollLeft -= groupWidth;
+    imgContainer.style.scrollBehavior = "smooth";
+    setTimeout(() => (isJumping = false), 0);
+  }
+
+  // find where the center is now
+  const centerX = imgContainer.scrollLeft + imgContainer.clientWidth / 2;
+  const halfCWidth = imgContainer.clientWidth / 2;
 
   // set the blur, scale and opacity
   imgs.forEach((img) => {
@@ -73,5 +96,92 @@ imgContainer.addEventListener("scroll", () => {
 // I firstly tried to copy 3 groups of pictures to fake the infinite scroll effect
 // the core is when scrolling to the bonding of final group, jump to first group;
 // however the jumping process is obvious.
-// I decide to use Js to clone the group (I recogonize that its the same effect as I copy them manually).
+
 // At this stage it looks quite like infinite scroll, except the annoying jumping flash.
+
+// ------------------3rd method----------------------
+// const imgContainer = document.querySelector(".imgs");
+// const slider = document.querySelector(".slider");
+// let imgs = [...document.querySelectorAll(".cards")];
+// let items = [...document.querySelectorAll(".sliderItem")];
+
+// let clones = [];
+// let containerWidth;
+// let groupWidth;
+// let scrollPos = 1;
+
+// // imgContainer.prepend(...imgs.map((img) => img.cloneNode(true)));
+// // imgContainer.append(...imgs.map((img) => img.cloneNode(true)));
+// // imgs = Array.from(imgContainer.querySelectorAll("img"));
+// items.forEach((item) => {
+//   let clone = item.cloneNode(true);
+//   clone.classList.add("clone");
+//   slider.appendChild(clone);
+//   clones.push(clone);
+// });
+
+// const gap = 0.05 * window.innerWidth;
+// const scrollRatio = 0.5;
+
+// function getImgWidth() {
+//   let width = 0;
+//   clones.forEach((div) => {
+//     width += div.offsetWidth;
+//   });
+//   return width;
+// }
+
+// function getScrollPosition() {
+//   return window.scrollY;
+// }
+
+// // map vertical scroll to horizontal scroll
+// function scrollUpdate() {
+//   scrollPos = getScrollPosition();
+
+//   if (scrollPos + groupWidth >= containerWidth) {
+//     document.documentElement.style.scrollBehavior = "auto";
+//     window.scrollTo({ top: 1 });
+//     document.documentElement.style.scrollBehavior = "smooth";
+//     // scrollPos = 1;
+//     // scrollPos -= groupWidth;
+//   } else if (scrollPos <= 0) {
+//     document.documentElement.style.scrollBehavior = "auto";
+//     window.scrollTo({ top: containerWidth - groupWidth - 1 });
+//     document.documentElement.style.scrollBehavior = "smooth";
+//     // scrollPos = groupWidth / scrollRatio - 1;
+//     // scrollPos += groupWidth;
+//   }
+
+//   slider.style.transform = `translateX(${-window.scrollY}px)`;
+//   //   clone.style.transform = `translateX(${
+//   //     -scrollPos * scrollRatio + groupWidth
+//   //   }px) translateY(-50%)`;
+
+//   requestAnimationFrame(scrollUpdate);
+// }
+
+// function onLoad() {
+//   calculateWidth();
+//   document.body.style.height = `${groupWidth}px`;
+//   //   window.scrollTo({ top: scrollPos / scrollRatio });
+//   scrollUpdate();
+// }
+
+// function calculateWidth() {
+//   groupWidth = slider.getBoundingClientRect().width;
+//   containerWidth = getImgWidth();
+// }
+
+// // requestAnimationFrame(scrollUpdate);
+
+// // window.addEventListener("scroll", () => {
+// //   scrollPos = window.scrollY;
+// // });
+
+// window.addEventListener("load", onLoad);
+
+// console.log(window.scrollY);
+// console.log(slider.scrollWidth);
+// console.log(document.body.style.height);
+// console.log(ratio);
